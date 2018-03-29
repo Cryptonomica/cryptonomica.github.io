4e09ba24a8aee2ca4845df73d898b2fda7efc99a
Viktor <ageyev@gmail.com>
2018-03-29 06:26:45 +0300

    add comments and formating

--- move.frontend.to.gae.sh
+++ move.frontend.to.gae.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
+# to to cryptonomica directory
 cd ../cryptonomica/src/main/webapp/
+# list files
 ls -latF
+
+# remove all files exept WEB-INF directory
 shopt -s extglob
 rm -rv !("WEB-INF")
 shopt -u extglob
+# check result
 ls -latF
+
+# return to cryptonomica.github.io directory
 cd ../../../../cryptonomica.github.io/
+
+# copy all files from cryptonomica.github.io:
 cp -rvu . ../cryptonomica/src/main/webapp/
+
+# remove cryptonomica.github.io specific files:
 rm -rfv ../cryptonomica/src/main/webapp/.idea
 rm -rfv ../cryptonomica/src/main/webapp/cryptonomica.github.io.iml
 rm -rfv ../cryptonomica/src/main/webapp/.gitignore
 rm -rfv ../cryptonomica/src/main/webapp/.git
+rm -rfv ../cryptonomica/src/main/webapp/move.frontend.to.gae.sh
+# show files in cryptonomica:
 ls -latF ../cryptonomica/src/main/webapp/
